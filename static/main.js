b46fdf52d01d4d1e2097da333d125f3949ce7f7f
Marwan Zibaoui <marwan.zibaoui@gmail.com>
2023-05-16 10:28:56 +0200

    Update display for number inputs

--- static/main.js
+++ static/main.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll("[data-number]");
+
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", (event) => {
+    appendNumber(event.target.innerHTML);
+  });
+});
+
+function appendNumber(numberStr) {
+  display.innerHTML += numberStr;
+}
+
+document.addEventListener("keydown", (keyboardEvent) => {
+  const { key } = keyboardEvent;
+  switch (key) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      appendNumber(key);
+      break;
+
+    default:
+      console.log(`pressed ${key}`);
+      break;
+  }
+});
